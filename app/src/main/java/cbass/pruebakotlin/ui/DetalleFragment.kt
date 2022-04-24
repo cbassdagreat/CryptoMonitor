6cc1731b569f22e6dc58a519af1c4bb075272fd9
Cbass <claveria.sebastian@gmail.com>
2022-04-24 14:40:17 -0400

    Initial commit

---
+++ app/src/main/java/cbass/pruebakotlin/ui/DetalleFragment.kt
@@ -0,0 +1,20 @@
+package cbass.pruebakotlin.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cbass.pruebakotlin.R
+
+class DetalleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detalle, container, false)
+    }
+
+}
